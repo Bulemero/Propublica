4ade642b2c83afda603c05cc4725a78363566129
Bulemero <bulemero@gmail.com>
2019-02-18 16:53:00 +0100

    updated attendance pages with vue.js and started to create fetch method using ajax call

--- vueTest.js
+++ vueTest.js
@@ -2,6 +2,8 @@ var myVueObject = new Vue({
     el: "#app",
     data: {
 
+        key: "FWHJZ1xefU7G9fkBIhnV9o021FA3btC0nLKaMjIT",
+        url: "https://api.propublica.org/congress/v1/116/",
         names: [
             {
                 party: "Democrats",
@@ -25,8 +27,45 @@ var myVueObject = new Vue({
             }
     ]
     },
-    methods: {
 
-    }
+    created: function () {
+        this.getData();
+    },
+
+    methods: {
+        getData: function () {
+
+            this.loading = true;
+
+            fetch(this.url, {
+                    headers: {
+                        "X-API-KEY": this.key
+                    }
+                })
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    this.members = data.results[0].members;
+                    this.loading = false;
+                })
+                .catch(error => console.log("Error: " + error));
+        }
+    },
+    /*computed: {
+        noRepeatedStates() {
+            return new Set(this.members.map(m => m.state).sort());
+        },
+        filteredMembers() {
+            return this.members.filter(member => {
+                let filter1 = this.checkBoxes.includes(member.party) || this.checkBoxes.length === 0;
+                let filter2 = member.state === this.stateSelected || this.stateSelected === "all";
+                return filter1 && filter2;
+            })
+        },
+        finalUrl() {
+            return "https://api.propublica.org/congress/v1/116/" + this.chamber + "/members.json"
+        }
+    }*/
 
 });
